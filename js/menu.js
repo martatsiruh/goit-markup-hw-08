432a9e44a7a350ff26ea9075ee0290368d4e777d
Marta Tsiruh <martatsiruch@gmail.com>
2021-11-01 21:10:58 +0100

    correction

--- js/menu.js
+++ js/menu.js
@@ -6,8 +6,8 @@
     const toggleMenu = () => {
       const isMenuOpen =
         openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('is-open');
+        openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+        mobileMenu.classList.toggle('is-open');
   
       const scrollLockMethod = !isMenuOpen
         ? 'disableBodyScroll'
@@ -15,12 +15,12 @@
       bodyScrollLock[scrollLockMethod](document.body);
     };
   
-    openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
+      openMenuBtn.addEventListener('click', toggleMenu);
+      closeMenuBtn.addEventListener('click', toggleMenu);
   
     // Закрываем мобильное меню на более широких экранах
     // в случае изменения ориентации устройства.
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+      window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
       if (!e.matches) return;
       mobileMenu.classList.remove('is-open');
       openMenuBtn.setAttribute('aria-expanded', false);
